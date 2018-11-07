e812cd0bb0e4fd1264700d278592e356dde3476f
Nav <navya.suri13@gmail.com>
2018-11-08 02:56:32 +0400

    Mushroom now plays sounds and destroys. New script created

---
+++ ForestGame/Assets/MushroomDestory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomDestory : MonoBehaviour {
+
+	public AudioClip collectSound;
+	AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(){
+		Debug.Log ("len" + collectSound.length);
+		Debug.Log ("Audio" + gameObject.GetComponent<AudioSource> ());
+		audio.PlayOneShot(collectSound, 0.8f);
+		GetComponent<Renderer> ().enabled = false;
+		Destroy (gameObject, collectSound.length);
+	}
+}
