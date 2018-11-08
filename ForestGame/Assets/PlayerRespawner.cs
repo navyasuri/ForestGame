6f2dcc51492e9ce1859a39cb98fc0df0a58b85fb
Nav <navya.suri13@gmail.com>
2018-11-08 11:04:40 +0400

    Added player respawn functionality

---
+++ ForestGame/Assets/PlayerRespawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.y < -50) {
+			gameObject.transform.position = new Vector3 (250, 30, 250);
+		}
+	}
+}
