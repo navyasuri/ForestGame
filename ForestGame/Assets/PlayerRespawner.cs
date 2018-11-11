de53d622e21274439a8ece787d8c23fa1e9971d7
Nav <navya.suri13@gmail.com>
2018-11-11 15:38:42 +0400

    Changes for final version

--- ForestGame/Assets/PlayerRespawner.cs
+++ ForestGame/Assets/PlayerRespawner.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerRespawner : MonoBehaviour {
 
+	Vector3 startPos = Vector3.zero;
+
 	// Use this for initialization
 	void Start () {
-		
+		startPos = GetComponent<Transform> ().position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (gameObject.transform.position.y < -50) {
-			gameObject.transform.position = new Vector3 (433, 35, 392);
+		if (gameObject.transform.position.y < -20) {
+			gameObject.transform.position = startPos;
 		}
 	}
 }
